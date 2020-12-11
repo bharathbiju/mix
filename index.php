c58ee9a5339a834aea394e4fe37348837878a00e
bharathbiju <bharathinfoclg@gmail.com>
2020-12-11 23:13:40 +0530

    adding php maile files

--- index.php
+++ index.php
@@ -68,6 +68,47 @@
 	</head>
 	
 	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+
+
+        <?php 
+		if(isset($_POST['sendmail'])) {
+			require 'PHPMailerAutoload.php';
+			require 'credential.php';
+
+			$mail = new PHPMailer;
+
+			// $mail->SMTPDebug = 4;                               // Enable verbose debug output
+
+			$mail->isSMTP();                                      // Set mailer to use SMTP
+			$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+			$mail->SMTPAuth = true;                               // Enable SMTP authentication
+			$mail->Username = EMAIL;                 // SMTP username
+			$mail->Password = PASS;                           // SMTP password
+			$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+			$mail->Port = 587;                                    // TCP port to connect to
+
+			$mail->setFrom(EMAIL, 'Dsmart Tutorials');
+			$mail->addAddress($_POST['email']);     // Add a recipient
+
+			$mail->addReplyTo(EMAIL);
+			// print_r($_FILES['file']); exit;
+			for ($i=0; $i < count($_FILES['file']['tmp_name']) ; $i++) { 
+				$mail->addAttachment($_FILES['file']['tmp_name'][$i], $_FILES['file']['name'][$i]);    // Optional name
+			}
+			$mail->isHTML(true);                                  // Set email format to HTML
+
+			$mail->Subject = $_POST['subject'];
+			$mail->Body    = '<div style="border:2px solid red;">This is the HTML message body <b>in bold!</b></div>';
+			$mail->AltBody = $_POST['message'];
+
+			if(!$mail->send()) {
+			    echo 'Message could not be sent.';
+			    echo 'Mailer Error: ' . $mail->ErrorInfo;
+			} else {
+			    echo 'Message has been sent';
+			}
+		}
+	 ?>
 		<div class="loader-container"><div class="loader"></div></div>
 		<header id="header">
 		
@@ -469,7 +510,7 @@
                      </div>
                      <div class="spacer" data-desktop_height="47" data-mobile_height="40" data-smobile_height="40" style="height: 47px;"></div>
                      <div class="form-submit">
-                         <form class="comment-form">
+                         <form class="comment-form" role="form" method="post" enctype="multipart/form-data">
                              <p class="comment-form-author">
                                  <!-- <label>Name *</label> -->
                                  <input name="author" type="text" placeholder="Your Name" required="required">
@@ -492,7 +533,7 @@
                                  <textarea name="comment" placeholder="Write Message" required="required"></textarea>
                              </p>
                              <p class="submit-form">
-                                 <button name="submit" type="submit" class="submit hvr-shutter-out-verticall">Send Message</button>
+                                 <button name="sendmail"  type="submit" class="submit hvr-shutter-out-verticall">Send Message</button>
                              </p>
                          </form>
                      </div>
